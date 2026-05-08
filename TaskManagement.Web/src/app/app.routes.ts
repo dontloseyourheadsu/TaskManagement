e126e312517a48ac77e8c0a4005857fed94f5f01
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-05-08 14:39:03 -0600

    Angular apply themes

--- TaskManagement.Web/src/app/app.routes.ts
+++ TaskManagement.Web/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login.component';
 import { RegisterComponent } from './components/auth/register.component';
 import { TaskViewerComponent } from './components/task-viewer/task-viewer.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: TaskViewerComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '**', redirectTo: '/login' }
 ];
